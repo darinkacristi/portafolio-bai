680314b6caadef46ab51553dafe4a14a70e02bb8
darinkacristi <darinkacristi.work@gmail.com>
2026-06-04 16:03:25 -0400

    Update projects.ts

--- data/projects.ts
+++ data/projects.ts
@@ -170,7 +170,6 @@ export function getProject(slug: string): Project | undefined {
 
 // Proyectos que aparecen en el inicio, en el orden que tú quieras (por slug)
 export const featuredSlugs: string[] = [
-  export const featuredSlugs: string[] = [
   "beneficios-del-tofu",
   "meflipa",
   "coterranea-ejecutivas",

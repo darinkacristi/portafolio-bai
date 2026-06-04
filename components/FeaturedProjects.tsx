bf238c895d1c5db394209d615ca4bab62d305246
darinkacristi <darinkacristi.work@gmail.com>
2026-06-04 16:12:26 -0400

    Update FeaturedProjects.tsx

--- components/FeaturedProjects.tsx
+++ components/FeaturedProjects.tsx
@@ -19,7 +19,7 @@ export default function FeaturedProjects() {
         </Link>
       </div>
 
-     <div className="columns-1 gap-4 sm:columns-2 lg:columns-4">
+     <div className="columns-1 gap-4 sm:columns-2 lg:columns-6">
         {featured.map((p, i) => (
           <ProjectCard key={p.slug} project={p} index={i} />
         ))}

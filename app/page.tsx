573e398233a2b689e75629b6a3ab546d5a16a8ae
darinkacristi <darinkacristi.work@gmail.com>
2026-06-10 18:12:12 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "@/components/Hero";
 import Marquee from "@/components/Marquee";
 import FeaturedProjects from "@/components/FeaturedProjects";
 import ClientsCarousel from "@/components/ClientsCarousel";
-// import Testimonials from "@/components/Testimonials";
+//import Testimonials from "@/components/Testimonials";
 import ContactCTA from "@/components/ContactCTA";
 import { Divider } from "@/components/Footer";
 
